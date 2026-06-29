1b5325d529c899dced640a4fdcd448a0b366029f
PhantomOblivion <smasif404@gmail.com>
2026-06-29 16:20:19 +0600

    config/db.js file section has been completed successfully.

---
+++ src/config/db.js
@@ -0,0 +1,30 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+const uri = process.env.MONGODB_URI;
+
+let db;
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+const connectDB = async () => {
+  try {
+    await client.connect();
+    db = client.db(process.env.DB_NAME);
+    console.log("✅ MongoDB Connected Successfully");
+  } catch (error) {
+    console.error("❌ MongoDB Connection Failed:", error);
+    process.exit(1);
+  }
+};
+
+const getDB = () => {
+  if (!db) throw new Error("DB not initialized. Call connectDB first.");
+  return db;
+};
+
+module.exports = { connectDB, getDB };
